19ff74346e0641f84f0cbd6b7be21b5a68dab99b
felipeoliveira25 <felipeoferreira.25@gmail.com>
2024-12-11 09:55:39 -0300

    feat: :sparkles: Introdução da configuração para receber os atributos dos produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import { stripe } from "@/lib/stripe";
+import { IProduct } from "@/types/product";
+import Image from "next/image";
 
 const getData = async() => {
-  const response = await stripe.products.list()
-  console.log(response.data)
-  return response.data
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+  const products:IProduct[] = response.data.map((product) => {
+    return {
+      id: product.id,
+      name: product.name,
+      imgUrl: product.images[0],
+    }
+  })
+  return products
 }
 
 export default async function Home() {
@@ -14,6 +24,7 @@ export default async function Home() {
       <ul>
         {data.map((item) => (
           <div key={item.id}>
+            <Image src={item.imgUrl} alt="" width={200} height={200}/>
             <span>{item.name}</span>
           </div>
         ))}
--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,10 +1,10 @@
 import Stripe from "stripe";
 
-if(!process.env.STRIPE_API_SECRET_KEY){
+if(!process.env.STRIPE_SECRET_KEY){
     throw new Error("A variável não está definida no ambiente")
 }
 
-export const stripe = new Stripe(process.env.STRIPE_API_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2024-11-20.acacia",
     appInfo: {
         name: "Palmeiras Shop"
